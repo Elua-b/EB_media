99b5e1bdaa149a24d58a75f3e646809f8a7880f5
Elua-b <bugingoeloi@gmail.com>
2022-07-22 17:31:08 +0200

    login a user

--- server/Routes/AuthRoutes.js
+++ server/Routes/AuthRoutes.js
@@ -1,9 +1,11 @@
 
 const express=require('express')
+const loginUser = require('../controllers/AuthController')
 const registerUser  = require('../controllers/AuthController')
 const router=express.Router()
 
 router.post('/register',registerUser)
+router.post('/login',loginUser)
  module.exports=router
 
   
--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 const registerUser = async (req, res) => {
   const { username, password, firstname, lastname } = req.body;
   const salt = await bcrypt.genSalt(10);
-  const hashedPass = await bcrypt.hash(password, salt);
+  const hashedPass = await bcrypt.hash(password,salt);
   const newUser = new UserModel({
     username,
     password: hashedPass,
@@ -17,4 +17,23 @@ const registerUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+const loginUser=async(req,res)=>{
+    const {username,password}=req.body
+
+    try {
+        const user=await UserModel.findOne({username:username})
+        if(user)
+        {
+          const validity=await bcrypt.compare(password,user.password) 
+          validity? res.status(200).json(user) :res.status(400).json("wrong password")
+
+        }
+        else{
+            res.status(404).json("user does not exists")
+        }
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+}
 module.exports = registerUser;
+module.exports=loginUser;
