6a95bc69b07b085ea1bcf7875acc3ed42a4d8d0e
Elua-b <bugingoeloi@gmail.com>
2022-07-25 23:54:57 +0200

    beautifying profile card

--- client/EB_media/src/components/ProfileCard.jsx
+++ client/EB_media/src/components/ProfileCard.jsx
@@ -11,6 +11,20 @@ function ProfileCard() {
             <span>BUGINGO</span>
             <span>Eloi</span>
         </div>
+        <div className="followStatus">
+            <hr />
+            <div>
+                <div className="follow">
+                    <span>3000</span>
+                    <span>followings</span>
+                </div>
+                <div className="vl"></div>
+                <div className="follow">
+                    <span>1</span>
+                    <span>followers</span>
+                </div>
+            </div>
+        </div>
     </div>
     )
 }
