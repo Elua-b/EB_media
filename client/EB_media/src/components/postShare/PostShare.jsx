a5343996fb65d4d2ad273e3359dbca87a554cf31
Elua-b <bugingoeloi@gmail.com>
2022-07-27 00:02:58 +0200

    adding useRef and useState and their events

--- client/EB_media/src/components/postShare/PostShare.jsx
+++ client/EB_media/src/components/postShare/PostShare.jsx
@@ -1,41 +1,69 @@
-import React from 'react'
-import './PostShare.css'
-import {UilScenery} from '@iconscout/react-unicons'
-import {UilPlayCircle} from '@iconscout/react-unicons'
-import {UilLocationPoint} from '@iconscout/react-unicons'
-import {UilSchedule} from '@iconscout/react-unicons'
+import React from "react";
+import "./PostShare.css";
+import { useState, useRef } from "react";
+import { UilScenery } from "@iconscout/react-unicons";
+import { UilPlayCircle } from "@iconscout/react-unicons";
+import { UilLocationPoint } from "@iconscout/react-unicons";
+import { UilSchedule } from "@iconscout/react-unicons";
+import { UilTimes } from "@iconscout/react-unicons";
+
 function PostShare() {
+  const [image, setImage] = useState(null);
+  const imageRef = useRef();
+  const onImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      let img = event.target.files[0];
+      setImage({
+        image: URL.createObjectURL(img),
+      });
+    }
+  };
   return (
-    <div className='PostShare'>
-        <img src="./jackie.PNG" alt="" />
-        <div>
-            <input type="text" placeholder="what's happening" />
-            <div className="postOptions">
-            <div className="option"
-            style={{color:"var(--photo"}}>
-                <UilScenery/>
-                photo
-            </div>
-            <div className="option"
-             style={{color:"var(--video"}}>
-                <UilPlayCircle/>
-                Video
-            </div>
-            <div className="option"
-             style={{color:"var(--location"}}>
-                <UilLocationPoint/>
-                Location
-            </div>
-            <div className="option"
-             style={{color:"var(--schedule"}}>
-                <UilSchedule/>
-                Schedule
-            </div>
+    <div className="PostShare">
+      <img src="./jackie.PNG" alt="" />
+      <div>
+        <input type="text" placeholder="what's happening" />
+        <div className="postOptions">
+          <div
+            className="option"
+            style={{ color: "var(--photo" }}
+            onClick={() => imageRef.current.click()}
+          >
+            <UilScenery />
+            photo
+          </div>
+          <div className="option" style={{ color: "var(--video" }}>
+            <UilPlayCircle />
+            Video
+          </div>
+          <div className="option" style={{ color: "var(--location" }}>
+            <UilLocationPoint />
+            Location
+          </div>
+          <div className="option" style={{ color: "var(--schedule" }}>
+            <UilSchedule />
+            Schedule
+          </div>
+          <button className="button ps-button">Share</button>
+          <div style={{ display: "none" }}>
+            <input
+              type="file"
+              name="myImage"
+              ref={imageRef}
+              onChange={onImageChange}
+            />
+          </div>
         </div>
-        </div>
-       
+        {image && [
+            <div className="previewImage">
+                <UilTimes/> 
+            </div>
+        ]
+
+        }
+      </div>
     </div>
-  )
+  );
 }
 
-export default PostShare
+export default PostShare;
