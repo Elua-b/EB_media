eac4846c920632d6fe618d1784a950277ebfc4c0
Elua-b <bugingoeloi@gmail.com>
2022-07-26 22:14:27 +0200

    the beginning of the PostSide

---
+++ client/EB_media/src/components/postSide/PostSide.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './PostSide.css'
+function PostSide() {
+  return (
+    <div>PostSide</div>
+  )
+}
+
+export default PostSide
--- client/EB_media/src/pages/home/Home.jsx
+++ client/EB_media/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import PostSide from "../../components/postSide/PostSide";
 import ProfileSide from "../../components/profileSide/ProfileSide";
 import './Home.css'
 function Home() {
   return <div className="Home">
     <ProfileSide/>
-    <div className="postSide">Posts</div>
+    {/* <div className="postSide">Posts</div> */}
+    <PostSide/>
     <div className="RightSide">RightSide</div>
   </div>;
 }
