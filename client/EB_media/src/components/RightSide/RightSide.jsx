f7751b5e0abf10a90fbc3b0aff63017dedd974c4
Elua-b <bugingoeloi@gmail.com>
2022-07-27 14:30:05 +0200

    adding the top side of the Right side

--- client/EB_media/src/components/RightSide/RightSide.jsx
+++ client/EB_media/src/components/RightSide/RightSide.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './RightSide.css'
+import {UilSetting} from '@iconscout/react-unicons'
 function RightSide() {
   return (
-    <div>
-      
+    <div className='RightSide'>
+        <div className="navIcons">
+            <img className='icon2' src="./home.png" alt="" />   
+            <UilSetting/> 
+            <img className='icon2' src="./not.png" alt="" />
+            <img className='icon2' src="./mess.png" alt="" /> 
+        </div>        
     </div>
   )
 }
