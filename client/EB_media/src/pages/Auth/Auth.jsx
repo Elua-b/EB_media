6d8eb1322ae1db8086d5ffa47084268f1bb6f6ba
Elua-b <bugingoeloi@gmail.com>
2022-08-02 22:21:57 +0200

    adding css

--- client/EB_media/src/pages/Auth/Auth.jsx
+++ client/EB_media/src/pages/Auth/Auth.jsx
@@ -4,12 +4,13 @@ function Auth() {
   return (
     <div className='Auth'>
       <div className="a-left">
-        <img src="./logo2.png" alt="" />
+        <img src="./logo2.png" className='logo' alt="" />
         <div className="webname">
             <h1>EB_media</h1>
             <h6>Explore the ideas throughout the world</h6>
         </div>
       </div>
+      <h1>form</h1>
     </div>
   )
 }
