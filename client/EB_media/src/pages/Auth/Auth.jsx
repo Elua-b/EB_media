086eecc1cb1e6332d01493c277dd5a549e7ba0bd
Elua-b <bugingoeloi@gmail.com>
2022-08-02 23:06:17 +0200

    adding inputs

--- client/EB_media/src/pages/Auth/Auth.jsx
+++ client/EB_media/src/pages/Auth/Auth.jsx
@@ -1,30 +1,63 @@
-import React from 'react'
-import './Auth.css'
+import React from "react";
+import "./Auth.css";
 function Auth() {
   return (
-    <div className='Auth'>
+    <div className="Auth">
       <div className="a-left">
-        <img src="./logo2.png" className='logo' alt="" />
+        <img src="./logo2.png" className="logo" alt="" />
         <div className="webname">
-            <h1 style={{color:"#f5c32c"}}>EB_media</h1>
-            <h6>Explore the ideas throughout the world</h6>
+          <h1 style={{ color: "#f5c32c" }}>EB_media</h1>
+          <h6>Explore the ideas throughout the world</h6>
         </div>
       </div>
-      <Signup/>
+      <Signup />
     </div>
-  )
+  );
 }
-function Signup(){
-    return(
-        <div className="a-right">
-          <form className="infoForm">
-            <h3>Sign up</h3>
-            <div>
-                <input type="text" placeholder='First Name' className='infoInput' name='firstname'/>
-            </div>
-            </form>  
+function Signup() {
+  return (
+    <div className="a-right">
+      <form className="infoForm">
+        <h3>Sign up</h3>
+        <div>
+          <input
+            type="text"
+            placeholder="First Name"
+            className="infoInput"
+            name="firstname"
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="infoInput"
+            name="lastname"
+          />
         </div>
-    )
+        <div>
+          <input
+            type="text"
+            className="infoInput"
+            name="username"
+            placeholder="Usernames"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            className="password"
+            name="password"
+            placeholder="Password"
+          />
+          <input
+          type="password"
+          className="password"
+          name="confirmpass"
+          placeholder="Confirm Password"
+        />
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Auth
+export default Auth;
