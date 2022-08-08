be5694ffeef1329e67640de3e31ec7838e7b7f1a
Elua-b <bugingoeloi@gmail.com>
2022-08-08 18:21:12 +0200

    adding content in the Modal

--- client/EB_media/src/components/profileModal/ProfileModal.jsx
+++ client/EB_media/src/components/profileModal/ProfileModal.jsx
@@ -1,20 +1,37 @@
-import { Modal, useMantineTheme } from '@mantine/core';
+import { Modal, useMantineTheme } from "@mantine/core";
 
-function ProfileModal({modalOpened,setModalOpened}) {
+function ProfileModal({ modalOpened, setModalOpened }) {
   const theme = useMantineTheme();
 
   return (
     <Modal
-      overlayColor={theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2]}
+      overlayColor={
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[9]
+          : theme.colors.gray[2]
+      }
       overlayOpacity={0.55}
       overlayBlur={3}
+      size='55%'
       opened={modalOpened}
-      onClose={()=>setModalOpened(false)}
+      onClose={() => setModalOpened(false)}
     >
       <form action="" className="infoForm">
         <h3>Your info</h3>
+        <div>
+            <input type="text"
+           className="infoInput"
+            name="FirstName"
+            placeholder="First Name"
+             />    
+            <input type="text"
+             className="infoInput"
+              name="LastName"
+              placeholder="Last Name"
+             />
+        </div>
       </form>
     </Modal>
   );
 }
-export default  ProfileModal
+export default ProfileModal;
