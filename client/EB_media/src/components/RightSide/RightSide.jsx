d5673c2e853c2b92c4d17a16a52d730f5ff560b4
Elua-b <bugingoeloi@gmail.com>
2022-08-08 18:49:04 +0200

    End of the frontend

--- client/EB_media/src/components/RightSide/RightSide.jsx
+++ client/EB_media/src/components/RightSide/RightSide.jsx
@@ -1,27 +1,25 @@
-import React ,{useState}from 'react'
-import './RightSide.css'
-import {UilSetting} from '@iconscout/react-unicons'
-import TrendCard from '../trendCard/TrendCard'
-import ShareModal from '../ShareModal/ShareModal';
+import React, { useState } from "react";
+import "./RightSide.css";
+import { UilSetting } from "@iconscout/react-unicons";
+import TrendCard from "../trendCard/TrendCard";
+import ShareModal from "../ShareModal/ShareModal";
 function RightSide() {
   const [modalOpened, setModalOpened] = useState(false);
   return (
-    <div className='RightSide'>
-        <div className="navIcons">
-            <img className='icon2' src="./home.png" alt="" />   
-            <UilSetting/> 
-            <img className='icon2' src="./not.png" alt="" />
-            <img className='icon2' src="./mess.png" alt="" /> 
-        </div>      
-        <TrendCard/>  
-        <button className="button r-button"   onClick={() => setModalOpened(true)}
-          >
-          <ShareModal modalOpened={modalOpened}
-          setModalOpened={setModalOpened} />
-            Share
-        </button>
+    <div className="RightSide">
+      <div className="navIcons">
+        <img className="icon2" src="./home.png" alt="" />
+        <UilSetting />
+        <img className="icon2" src="./not.png" alt="" />
+        <img className="icon2" src="./mess.png" alt="" />
+      </div>
+      <TrendCard />
+      <button className="button r-button" onClick={() => setModalOpened(true)}>
+        Share
+      </button>
+      <ShareModal modalOpened={modalOpened} setModalOpened={setModalOpened} />
     </div>
-  )
+  );
 }
 
-export default RightSide
+export default RightSide;
