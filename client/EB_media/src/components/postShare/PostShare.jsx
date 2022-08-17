fac5dd2152ae0521040fcc6afc48d0f3dcffdb0a
Elua-b <bugingoeloi@gmail.com>
2022-08-17 23:29:42 +0200

    adding posting functions

--- client/EB_media/src/components/postShare/PostShare.jsx
+++ client/EB_media/src/components/postShare/PostShare.jsx
@@ -6,23 +6,39 @@ import { UilPlayCircle } from "@iconscout/react-unicons";
 import { UilLocationPoint } from "@iconscout/react-unicons";
 import { UilSchedule } from "@iconscout/react-unicons";
 import { UilTimes } from "@iconscout/react-unicons";
-
+import { useSelector } from "react-redux";
 function PostShare() {
   const [image, setImage] = useState(null);
   const imageRef = useRef();
+  const desc=useRef()
+  const {user}=useSelector((state)=>state.authReducer.authData)
+  console.log(user);
   const onImageChange = (event) => {
     if (event.target.files && event.target.files[0]) {
       let img = event.target.files[0];
-      setImage({
-        image: URL.createObjectURL(img),
-      });
+      setImage(img);
     }
   };
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const newPost={
+    userId:user.id, 
+    desc:desc.current.value  
+    }
+    if(image){
+      const data=new FormData();
+      const filename=Date.now() + image.name
+      data.append("name",filename)
+      data.append("file",image)
+      newPost.image=filename;
+      console.log(newPost);
+    }
+  }
   return (
     <div className="PostShare">
       <img src="./jackie.PNG" alt="" />
       <div>
-        <input type="text" placeholder="what's happening" />
+        <input ref={desc}  required type="text" placeholder="what's happening" />
         <div className="postOptions">
           <div
             className="option"
@@ -44,7 +60,7 @@ function PostShare() {
             <UilSchedule />
             Schedule
           </div>
-          <button className="button ps-button">Share</button>
+          <button className="button ps-button" onClick={handleSubmit}>Share</button>
           <div style={{ display: "none" }}>
             <input
               type="file"
@@ -55,13 +71,11 @@ function PostShare() {
           </div>
         </div>
         {image && [
-            <div className="previewImage">
-                <UilTimes onClick={()=>setImage(null)}/> 
-                <img src={image.image} alt="" />
-            </div>
-        ]
-
-        }
+          <div className="previewImage">
+            <UilTimes onClick={() => setImage(null)} />
+            <img src={URL.createObjectURL(image)} alt="" />
+          </div>,
+        ]}
       </div>
     </div>
   );
